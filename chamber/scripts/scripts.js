1c23a148cb4d95b5dfd4f30e0f7599b9717a999a
jenniferkleon <jenniferkleon@outlook.com>
2023-01-28 22:42:43 -0800

    update-home

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,6 +1,17 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
-
-
-
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
